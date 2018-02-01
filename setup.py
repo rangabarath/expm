9a1980bd4cf49a78c3ca7606569b0e41f6447006
nrahmani <rahmani_nilofar@bah.com>
2018-02-01 13:20:14 -0600

    adding commas

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import os
 from setuptools import setuptools
 
 setup(
-	name = "expm"
-	version = "0.0.1"
-	author = "Nilofar Rahmani"
-	author_email = "nilofar.z.rahmani@exxommobil.com"
-	description = "Installs required packages"
-	url = "https://github.com/ranganathanbarath/expm"
+	name = "expm",
+	version = "0.0.1",
+	author = "Nilofar Rahmani",
+	author_email = "nilofar.z.rahmani@exxommobil.com",
+	description = "Installs required packages",
+	url = "https://github.com/ranganathanbarath/expm",
 )
