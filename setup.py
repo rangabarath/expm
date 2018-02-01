d5bb1de4d92dee583b5559f6372453f863ff0bfd
nrahmani <rahmani_nilofar@bah.com>
2018-02-01 10:34:08 -0600

    setting up setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setuptools
+
+setup(
+	name = "expm"
+	version = "0.0.1"
+	author = "Nilofar Rahmani"
+	author_email = "nilofar.z.rahmani@exxommobil.com"
+	description = "Installs required packages"
+	url = "https://github.com/ranganathanbarath/expm"
+)
